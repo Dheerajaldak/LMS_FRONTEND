3d8c2488c43a56894363763bbd95e4d99cb01d4e
Dheeraj Aldak <144196534+Dheerajaldak@users.noreply.github.com>
2025-03-27 10:56:12 +0530

    Update axiosinstance.js backend url

--- src/Helpers/axiosinstance.js
+++ src/Helpers/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5014/api/v1";
-
+// const BASE_URL = "http://localhost:5014/api/v1";
+const BASE_URL = "https://lms-backend-s99f.onrender.com/api/v1";
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL=BASE_URL;
